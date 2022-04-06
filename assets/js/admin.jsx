9727d73bfb0ac0bbca0c8a8b0ce452eb62679305
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2022-04-06 13:33:54 -0300

    refactor: replace container with wrapper utility

--- assets/js/admin.jsx
+++ assets/js/admin.jsx
@@ -7,44 +7,40 @@ class EpisodesPreview extends React.Component {
     const { entry, widgetFor } = this.props;
 
     return (
-      <div class="container">
-        <div class="row hero_container middle_container">
-          <div class="col-md-12">
-            <div class="row title">
-              <div class="col">
-                <h1>
-                  <a href="#">
-                    <h1>
-                      #{entry.getIn(["data", "episode"])}{" "}
-                      {entry.getIn(["data", "title"])}
-                    </h1>
-                  </a>
-                </h1>
-              </div>
+      <div class="row hero_container middle_container wrapper">
+        <div class="col-md-12">
+          <div class="row title">
+            <div class="col">
+              <h1>
+                <a href="#">
+                  #{entry.getIn(["data", "episode"])}{" "}
+                  {entry.getIn(["data", "title"])}
+                </a>
+              </h1>
             </div>
-            <div class="row description-content">
-              <div class="col">
-                <div style={{ "margin-bottom": "12px" }}>
-                  <h2>cabeça</h2>
-                  {entry.getIn(["data", "description"])}
-                </div>
-                <div>{widgetFor("body")}</div>
+          </div>
+          <div class="row description-content">
+            <div class="col">
+              <div style={{ "margin-bottom": "12px" }}>
+                <h2>cabeça</h2>
+                {entry.getIn(["data", "description"])}
               </div>
+              <div>{widgetFor("body")}</div>
             </div>
+          </div>
 
-            <div class="row youtube_row">
-              <div class="col">
-                <h2>Escute no youtube:</h2>
-                <div class="embed-responsive embed-responsive-1by1">
-                  <iframe
-                    class="embed-responsive-item"
-                    src={`//www.youtube.com/embed/${entry.getIn([
-                      "data",
-                      "youtube",
-                    ])}`}
-                    allowFullScreen=""
-                  ></iframe>
-                </div>
+          <div class="row youtube_row">
+            <div class="col">
+              <h2>Escute no youtube:</h2>
+              <div class="embed-responsive embed-responsive-1by1">
+                <iframe
+                  class="embed-responsive-item"
+                  src={`//www.youtube.com/embed/${entry.getIn([
+                    "data",
+                    "youtube",
+                  ])}`}
+                  allowFullScreen=""
+                ></iframe>
               </div>
             </div>
           </div>
