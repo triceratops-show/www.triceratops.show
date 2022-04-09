155c3c2f7868c2bd0358e1a615af8a97b1210170
Daniela Yassuda <danielayassuda@gmail.com>
2022-04-09 13:45:47 -0300

    feat: design tokens (#63)
    * chore: add jsconfig.json to gitignore
    
    * chore(assets): fonts
    
    * feat(assets): design tokens and gorko
    
    * refactor: design tokens
    
    * chore(assets): rename to scss
    
    * refactor: replace container with wrapper utility
    
    * feat: 404 alt text
    
    * fix: list role and labels
    
    * feat: footer
    
    * feat: non-fixed header
    
    * chore(assets): remove margin utilities
    
    * chore(assets): remove dropdown block
    
    * refactor: prose & section
    
    * fix(content): participacoes file name
    
    * refactor: episode
    
    * feat(assets): link active color
    
    * feat(layouts): default list
    
    * fix(assets): prevent featured title from overlapping
    
    * feat(layouts): render heading with anchor link
    
    * refactor: episode continued
    
    * fix: anchor link position
    
    * chore: delete css!!!
    
    * refactor: move heading link style to prose
    
    * refactor: heading links

--- assets/js/admin.jsx
+++ assets/js/admin.jsx
@@ -7,46 +7,30 @@ class EpisodesPreview extends React.Component {
     const { entry, widgetFor } = this.props;
 
     return (
-      <div class="container">
-        <div class="row hero_container middle_container">
-          <div class="col-md-12">
-            <div class="row title">
-              <div class="col">
-                <h1>
-                  <a href="#">
-                    <h1>
-                      #{entry.getIn(["data", "episode"])}{" "}
-                      {entry.getIn(["data", "title"])}
-                    </h1>
-                  </a>
-                </h1>
-              </div>
-            </div>
-            <div class="row description-content">
-              <div class="col">
-                <div style={{ "margin-bottom": "12px" }}>
-                  <h2>cabeça</h2>
-                  {entry.getIn(["data", "description"])}
-                </div>
-                <div>{widgetFor("body")}</div>
-              </div>
-            </div>
+      <div class="section prose">
+        <h1>
+          <a href="https://wwww.triceratops.show">
+            #{entry.getIn(["data", "episode"])}{" "}
+            {entry.getIn(["data", "title"])}
+          </a>
+        </h1>
 
-            <div class="row youtube_row">
-              <div class="col">
-                <h2>Escute no youtube:</h2>
-                <div class="embed-responsive embed-responsive-1by1">
-                  <iframe
-                    class="embed-responsive-item"
-                    src={`//www.youtube.com/embed/${entry.getIn([
-                      "data",
-                      "youtube",
-                    ])}`}
-                    allowFullScreen=""
-                  ></iframe>
-                </div>
-              </div>
-            </div>
+        <h2 id="#cabeça"><a href="#cabeça">cabeça</a></h2>
+        <p>{entry.getIn(["data", "description"])}</p>
+
+        {widgetFor("body")}
+
+        <div class="all-center flow">
+          <h2>Escute no youtube:</h2>
+          <div class="embed responsive-1by1 max-width">
+            <iframe
+              class="responsive-item"
+              src={`//www.youtube.com/embed/${entry.getIn([
+                "data",
+                "youtube",
+              ])}`}
+              allowFullScreen=""
+            ></iframe>
           </div>
         </div>
       </div>
